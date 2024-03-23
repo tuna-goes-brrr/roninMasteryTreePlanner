ec5d8375538e912b33ec5ca55b8fac69ed047cc6
Tuna <>
2024-03-23 08:59:29 +0700

    fix some more js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,30 +1,39 @@
-const mstrXMLhttp = new XMLHttpRequest();
-mstrXMLhttp.open("GET", "https://tuna-goes-brrr.github.io/roninMasteryTreePlanner/resources/data/masteryData.json");
-mstrXMLhttp.onload = function () {
+const XMLhttp = new XMLHttpRequest();
+XMLhttp.open("GET", "https://tuna-goes-brrr.github.io/roninMasteryTreePlanner/resources/data/masteryData.json");
+XMLhttp.onload = function () {
     const masteryTree = JSON.parse(this.responseText);
     alert(this);
 }
-mstrXMLhttp.send();
-
-function updateMasteryDetail(msg) {
-    document.getElementById("masteryDetail").innerHTML = msg;
+XMLhttp.send();
+XMLhttp.open("GET", "https://tuna-goes-brrr.github.io/roninMasteryTreePlanner/resources/data/lang.json");
+XMLhttp.onload = function () {
+    const lang = JSON.parse(this.responseText);
+    alert(this);
 }
-function updateCharacter(ronin) {
-    // update lại ảnh các nhánh lão luyện của từng char
-    if (ronin == "Kenji") {
-        document.getElementById("red2Name").innerHTML = "Speed";
-        document.getElementById("blue2Name").innerHTML = "Boss DMG";
-    } else if (ronin == "Tomoe") {
-        document.getElementById("red2Name").innerHTML = "CRIT DMG";
-        document.getElementById("blue2Name").innerHTML = "CRIT Rate";
-    }
-}
+XMLhttp.send();
 
+let region = "en-US";
 let skillValue = {
     red: [0, 0, 0, 0, 0],
     blue: [0, 0, 0, 0, 0],
     green: [0, 0, 0, 0, 0]
 }
+
+function updateMasteryDetail(msg) {
+    document.getElementById("masteryDetail").innerHTML = msg;
+}
+
+const ele = document.querySelector("");
+ele.addEventListener("change", function () {
+    if (this == "Kenji") {
+        document.getElementById("red2Name").innerHTML = lang[region].greed.skills.shortname[1];
+        document.getElementById("blue2Name").innerHTML = lang[region].mastermind.skills.shortname[1];
+    } else if (ronin == "Tomoe") {
+        document.getElementById("red2Name").innerHTML = lang[region].greed.tomoe.skills.shortname[1];
+        document.getElementById("blue2Name").innerHTML = lang[region].mastermind.tomoe.skills.shortname[1];
+    }
+});
+
 const skillNames = document.querySelectorAll('.skillBlock .skillName');
 const skillValues = document.querySelectorAll(".skillBlock .skillValue");
 for (let i = 0; i < skillNames.length; i++) {
